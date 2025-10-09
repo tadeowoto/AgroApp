676de19cedd567abca317b2a031d389b5ee20522
Tadeo Wotoszyn <tadeowotoszyn@MacBook-Pro-de-Tadeo.local>
2025-10-09 11:32:04 -0300

    feat: agregue los listener a los botones de cada una de las gestiones

--- app/src/main/java/com/example/agroapp/menu/ui/gestion/GestionFragment.java
+++ app/src/main/java/com/example/agroapp/menu/ui/gestion/GestionFragment.java
@@ -7,32 +7,70 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.agroapp.R;
+import com.example.agroapp.databinding.FragmentGestionBinding;
 
 public class GestionFragment extends Fragment {
 
-    private GestionViewModel mViewModel;
-
-    public static GestionFragment newInstance() {
-        return new GestionFragment();
-    }
+    private GestionViewModel vm;
+    private FragmentGestionBinding binding;
 
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_gestion, container, false);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        vm = ViewModelProvider.AndroidViewModelFactory.getInstance(requireActivity().getApplication()).create(GestionViewModel.class);
+        binding = FragmentGestionBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+
+        binding.btnActividades.setOnClickListener(v -> {
+            // abrir el fragment de actividades
+            Bundle bundle = new Bundle();
+            Navigation.findNavController(getActivity(), R.id.nav_host_fragment_content_menu).navigate(R.id.action_gestionFragment_to_actividadFragment);
+        });
+
+        binding.btnCampos.setOnClickListener(v -> {
+            // abrir el fragment de campos
+            Bundle bundle = new Bundle();
+            Navigation.findNavController(getActivity(), R.id.nav_host_fragment_content_menu).navigate(R.id.action_gestionFragment_to_campoFragment);
+        });
+
+        binding.btnCosechas.setOnClickListener(v -> {
+            // abrir el fragment de cosechas
+            Bundle bundle = new Bundle();
+            Navigation.findNavController(getActivity(), R.id.nav_host_fragment_content_menu).navigate(R.id.action_gestionFragment_to_cosechaFragment);
+        });
+
+        binding.btnInsumos.setOnClickListener(v -> {
+            // abrir el fragment de insumos
+            Bundle bundle = new Bundle();
+            Navigation.findNavController(getActivity(), R.id.nav_host_fragment_content_menu).navigate(R.id.action_gestionFragment_to_insumoFragment);
+        });
+
+        binding.btnLotes.setOnClickListener(v -> {
+            // abrir el fragment de lotes
+            Bundle bundle = new Bundle();
+            Navigation.findNavController(getActivity(), R.id.nav_host_fragment_content_menu).navigate(R.id.action_gestionFragment_to_loteFragment);
+        });
+
+        binding.btnRecursos.setOnClickListener(v -> {
+            // abrir el fragment de recursos
+            Bundle bundle = new Bundle();
+            Navigation.findNavController(getActivity(), R.id.nav_host_fragment_content_menu).navigate(R.id.action_gestionFragment_to_recursoFragment);
+        });
+
+
+
+        return root;
+
     }
 
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        mViewModel = new ViewModelProvider(this).get(GestionViewModel.class);
-        // TODO: Use the ViewModel
-    }
+
 
 }
