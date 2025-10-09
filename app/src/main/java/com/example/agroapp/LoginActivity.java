8b231adb3fed43e14e4b74080547c120884958bc
Tadeo Wotoszyn <tadeowotoszyn@MacBook-Pro-de-Tadeo.local>
2025-10-09 10:58:25 -0300

    feat: agregue login harcoded

--- app/src/main/java/com/example/agroapp/LoginActivity.java
+++ app/src/main/java/com/example/agroapp/LoginActivity.java
@@ -1,6 +1,9 @@
 package com.example.agroapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +13,7 @@ import androidx.core.view.WindowInsetsCompat;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.agroapp.databinding.ActivityLoginBinding;
+import com.example.agroapp.menu.MenuActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -25,7 +29,31 @@ public class LoginActivity extends AppCompatActivity {
         vm = ViewModelProvider.AndroidViewModelFactory.getInstance(getApplication()).create(LoginActivityViewModel.class);
         binding = ActivityLoginBinding.inflate(getLayoutInflater());
 
-        setContentView(R.layout.activity_login);
+
+        binding.btnLogin.setOnClickListener(v -> {
+            String usuario = binding.etUsuario.getText().toString();
+            String password = binding.etPassword.getText().toString();
+            vm.validarUsuario(usuario, password);
+        });
+
+        vm.getErrorNombre().observe(this, error -> {
+            binding.tvErrorUsuario.setText(error);
+            binding.tvErrorUsuario.setVisibility((View.VISIBLE));
+        });
+
+        vm.getErrorPassword().observe(this, error -> {
+            binding.tvErrorPassword.setText(error);
+            binding.tvErrorPassword.setVisibility(View.VISIBLE);
+        });
+
+        vm.getLoginExitoso().observe(this, loginExitoso -> {
+            Toast.makeText(this, "Login correcto", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(this, MenuActivity.class);
+            startActivity(intent);
+        });
+
+
+        setContentView(binding.getRoot());
 
     }
 }
