124dddab2f19625f71ac8834846d99b2d681d176
Tadeo Wotoszyn <tadeowotoszyn@MacBook-Pro-de-Tadeo.local>
2025-10-23 11:21:18 -0300

    fix: cambie la entidad para no obtener la password

--- app/src/main/java/com/example/agroapp/model/Usuario.java
+++ app/src/main/java/com/example/agroapp/model/Usuario.java
@@ -5,14 +5,12 @@ public class Usuario {
     private int id_usuario;
     private String nombre;
     private String email;
-    private String password;
     private String telefono;
 
-    public Usuario(int id_usuario, String nombre, String email, String password, String telefono) {
+    public Usuario(int id_usuario, String nombre, String email, String telefono) {
         this.id_usuario = id_usuario;
         this.nombre = nombre;
         this.email = email;
-        this.password = password;
         this.telefono = telefono;
     }
 
@@ -40,13 +38,6 @@ public class Usuario {
         this.email = email;
     }
 
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 
     public String getTelefono() {
         return telefono;
