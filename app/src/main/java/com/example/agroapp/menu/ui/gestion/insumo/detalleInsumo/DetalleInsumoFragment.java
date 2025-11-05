d613d374bdb198263818dac062f6e7d978de0363
Tadeo Wotoszyn <tadeowotoszyn@MacBook-Pro-de-Tadeo.local>
2025-11-05 13:56:22 -0300

    feat: agregue el detalle de el insumo

--- app/src/main/java/com/example/agroapp/menu/ui/gestion/insumo/detalleInsumo/DetalleInsumoFragment.java
+++ app/src/main/java/com/example/agroapp/menu/ui/gestion/insumo/detalleInsumo/DetalleInsumoFragment.java
@@ -11,9 +11,14 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 
 import com.example.agroapp.R;
 import com.example.agroapp.databinding.FragmentDetalleInsumoBinding;
+import com.example.agroapp.model.insumo.Insumo;
+
+import java.text.SimpleDateFormat;
+import java.util.Locale;
 
 public class DetalleInsumoFragment extends Fragment {
 
@@ -28,6 +33,59 @@ public class DetalleInsumoFragment extends Fragment {
 
         binding = FragmentDetalleInsumoBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+
+        Insumo insumo = (Insumo) getArguments().getSerializable("insumo");
+
+        vm.cargarInsumo(insumo);
+
+        vm.getInsumo().observe(getViewLifecycleOwner(), i -> {
+
+                binding.etNombre.setText(i.getNombre());
+                binding.spTipo.setText(i.getTipo(), false);
+                binding.spUnidad.setText(i.getUnidad(), false);
+                binding.etStock.setText(String.format(Locale.getDefault(), "%.2f", i.getStock_actual()));
+                    SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault());
+                    String fechaFormateada = sdf.format(i.getFecha_vencimiento());
+                    binding.etFechaVenc.setText(fechaFormateada);
+        });
+
+        vm.getmListaTipo().observe(getViewLifecycleOwner(), tipos -> {
+            ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_dropdown_item_1line, tipos);
+            binding.spTipo.setAdapter(adapter);
+        });
+        vm.getmListaUnidad().observe(getViewLifecycleOwner(), unidades -> {
+            ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_dropdown_item_1line, unidades);
+            binding.spUnidad.setAdapter(adapter);
+        });
+
+
+        vm.getHabilitarCampos().observe(getViewLifecycleOwner(), habilitar -> {
+            binding.etNombre.setEnabled(habilitar);
+            binding.spTipo.setEnabled(habilitar);
+            binding.spUnidad.setEnabled(habilitar);
+            binding.etStock.setEnabled(habilitar);
+            binding.etFechaVenc.setEnabled(habilitar);
+        });
+
+        vm.getGuardarInsumo().observe(getViewLifecycleOwner(), guardar -> {
+            String nombre = binding.etNombre.getText().toString();
+            String tipo = binding.spTipo.getText().toString();
+            String unidad = binding.spUnidad.getText().toString();
+            String stock = binding.etStock.getText().toString();
+            String fechaVenc = binding.etFechaVenc.getText().toString();
+
+            vm.editarInsumo(nombre, tipo, unidad, stock, fechaVenc);
+
+        });
+
+        vm.getTextoBoton().observe(getViewLifecycleOwner(), texto -> {
+            binding.btnEditarInsumo.setText(texto);
+        });
+
+        binding.btnEditarInsumo.setOnClickListener(v -> {
+            vm.procesarBoton(binding.btnEditarInsumo.getText().toString());
+        });
 
 
 
--- app/src/main/java/com/example/agroapp/menu/ui/gestion/insumo/detalleInsumo/DetalleInsumoViewModel.java
+++ app/src/main/java/com/example/agroapp/menu/ui/gestion/insumo/detalleInsumo/DetalleInsumoViewModel.java
@@ -4,11 +4,84 @@ import android.app.Application;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
+
+import com.example.agroapp.model.insumo.Insumo;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class DetalleInsumoViewModel extends AndroidViewModel {
 
+    private MutableLiveData<Insumo> mInsumo = new MutableLiveData<>();
+    private MutableLiveData<String> mError = new MutableLiveData<>();
+    private MutableLiveData<Boolean> mHabilitarCampos = new MutableLiveData<>();
+    private MutableLiveData<String> mTextoBoton = new MutableLiveData<>();
+    private MutableLiveData<Boolean> mGuardarInsumo  = new MutableLiveData<>();
+    private MutableLiveData<List<String>> mListaTipo = new MutableLiveData<>();
+    private MutableLiveData<List<String>> mListaUnidad = new MutableLiveData<>();
+
+
+
+
+
+
     public DetalleInsumoViewModel(@NonNull Application application) {
         super(application);
+        cargarListas();
+    }
+
+    public LiveData<List<String>> getmListaTipo() {
+        return mListaTipo;
+    }
+    public LiveData<List<String>> getmListaUnidad() {
+        return mListaUnidad;
+    }
+    public LiveData<Insumo> getInsumo() {
+        return mInsumo;
+    }
+    public LiveData<String> getError() {
+        return mError;
+    }
+    public LiveData<Boolean> getHabilitarCampos() {
+        return mHabilitarCampos;
+    }
+    public LiveData<String> getTextoBoton() {
+        return mTextoBoton;
+    }
+    public LiveData<Boolean> getGuardarInsumo() {
+        return mGuardarInsumo;
+    }
+
+
+    private void cargarListas() {
+        List<String> unidades = Arrays.asList("Kilos", "Libras", "Gramos", "Litros", "Metros", "Bolsas");
+        mListaUnidad.setValue(unidades);
+        List<String> tipos = Arrays.asList("Alimento", "Herbicida", "Fungicida", "Fertilizante", "Semillas");
+        mListaTipo.setValue(tipos);
+    }
+
+    public void procesarBoton(String textoBoton){
+        if (textoBoton.equals("Editar")){
+            mHabilitarCampos.setValue(true);
+            mTextoBoton.setValue("Guardar");
+        }else{
+            mGuardarInsumo.setValue(true);
+            mHabilitarCampos.setValue(false);
+            mTextoBoton.setValue("Editar");
+        }
+    }
+
+
+
+    public void cargarInsumo(Insumo insumo) {
+        mInsumo.setValue(insumo);
+    }
+
+
+
+    public void editarInsumo(String nombre, String tipo, String unidad, String stock, String fechaVenc) {
     }
 }
